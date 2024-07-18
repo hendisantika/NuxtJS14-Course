c3d603fe8da0c77c490fb4d772aa969d10199c50
hendisantika <hendisantika@yahoo.co.id>
2024-07-18 21:23:39 +0700

    Modify page.tsx with router🫸🌀✏️📗🐧🐳

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,13 @@
 "use client"
-import {useParams} from "next/navigation";
+import {useParams, useRouter} from "next/navigation";
 
 export default function profileID() {
     const params = useParams();
-    return <div>Profile page ID: {params.id}</div>;
+    const router = useRouter();
+    return (
+        <div>
+            <h1>Profile page ID: {params.id}</h1>
+            <div onClick={() => router.push("/profile")}>Back to main page</div>
+        </div>
+    );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,18 @@
+"use client"
+
+import {useRouter} from "next/navigation";
+
 export default function ProfilePage() {
+    const router = useRouter()
+
+    console.log(router)
     return <div>
         <h1>My List of profiles</h1>
         <div>
             <ul>
-                <li>Profile 1</li>
-                <li>Profile 2</li>
-                <li>Profile 3</li>
+                <li onClick={() => router.push("/profile/1")}>Profile 1</li>
+                <li onClick={() => router.push("/profile/2")}>Profile 2</li>
+                <li onClick={() => router.push("/profile/3")}>Profile 3</li>
             </ul>
         </div>
     </div>;
