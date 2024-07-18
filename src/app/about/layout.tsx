60185074905a7074a625465ffd560e11e20231ac
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 06:49:22 +0700

    Learn Layout🫸🌀✏️📗🐧🐳

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../about.css"
+
+export default function AboutLayout({
+                                        children,
+                                    }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html>
+        <body>
+        <header>This is the about header</header>
+        {children}
+        </body>
+        </html>
+    );
+}
