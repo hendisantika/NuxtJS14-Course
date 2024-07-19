8e5b7ddbef012d3fff4685486f7dd73008dd5b3a
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 08:37:24 +0700

    Learn Metadata🫸🌀✏️📗🐧🐳

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
-"use client"
+// export const metadata: Metadata = {
+//     title: "My page title",
+//     description: "This is the description of the application.",
+// };
 
-import Image from "next/image";
-import {useState} from "react";
 
 export default function Home() {
-    const [loading, setLoading] = useState(false);
-    const fetchDataFromAPI = async () => {
-        try {
-            setLoading(true);
-            const response = await fetch("/api/users", {
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                    method: "GET"
-                },
-            });
-            if (response) {
-                const data = await response.json();
-                console.log(data);
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    // const [loading, setLoading] = useState(false);
+    // const fetchDataFromAPI = async () => {
+    //     try {
+    //         setLoading(true);
+    //         const response = await fetch("/api/users", {
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 Accept: "application/json",
+    //                 method: "GET"
+    //             },
+    //         });
+    //         if (response) {
+    //             const data = await response.json();
+    //             console.log(data);
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     } finally {
+    //         setLoading(false);
+    //     }
+    // };
     return (
         <div>
-            <Image src={"/hendi.jpeg"} width={200} height={500} alt="Me at Photo studio"></Image>
-      </div>
-  );
+            <main>Main Page</main>
+            ;
+        </div>
+
+    )
+    // <Image src={"/hendi.jpeg"} width={200} height={500} alt="Me at Photo studio"></Image>*/}
 }
