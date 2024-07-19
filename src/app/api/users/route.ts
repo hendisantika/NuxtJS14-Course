7b1d6241800ca83f001130c625bb7b7dd221d934
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 08:07:04 +0700

    Learn Routes API :two:🫸🌀✏️📗🐧🐳

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,15 @@
+import {NextResponse} from "next/server";
+import data from '@/data.json';
+
+export async function GET() {
+    return NextResponse.json({
+        data,
+    });
+}
+
+export async function POST(request: Request) {
+    const data = await request.json()
+    return NextResponse.json({
+        data,
+    })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
 "use client"
 
 import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
+    const [loading, setLoading] = useState(false);
+    const fetchDataFromAPI = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch("/api/users", {
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    method: "GET"
+                },
+            });
+            if (response) {
+                const data = await response.json();
+                console.log(data);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <div>
             <Image src={"/hendi.jpeg"} width={200} height={500} alt="Me at Photo studio"></Image>
