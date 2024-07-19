ecf2063cea9d4310acfa51d311c0da7777c93055
hendisantika <hendisantika@yahoo.co.id>
2024-07-19 07:01:06 +0700

    Learn Image Component🫸🌀✏️📗🐧🐳

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <div>
-            <Header name={"Hendi"} age={36} isMan={true}>
-                <p>A Message from Home</p>
-            </Header>
+            <Image src={"/hendi.jpeg"} width={200} height={500} alt="Me at Photo studio"></Image>
       </div>
   );
 }
